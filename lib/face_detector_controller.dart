e5d5e8f2807dbb14ff7b2956f4cca558b4dc867b
paulo <pauloserp@github.com>
2020-07-06 12:12:51 -0300

    Configurando o FaceDetector

--- lib/face_detector_controller.dart
+++ lib/face_detector_controller.dart
@@ -1,49 +1,59 @@
 import 'dart:io';
-import 'dart:math';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
-class FaceDetectorController{
+class FaceDetectorController {
 
-  pickImage(File image)async{
+  pickImage(File image) async {
+
+    ///cria uma instancia de FirebaseVisionImage e coloca o file que vc recebeu na criação FirebaseVisionImage.fromFile(image).
     final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(image);
     print('VisionImage: $image');
-    final FaceDetector faceDetector = FirebaseVision.instance.faceDetector();
-    final List<Face> faces = await faceDetector.processImage(visionImage);
-    print('ListFace: ${faces.length}');
 
+    ///depois precisa abilitar as opções do detector, porque por default ele vem tudo false, isso eu vi lá na DOC do firebase mas eles não explicam então precisei vir aqui na API e vê
+    ///vc vai encontrar uma explicação em face_detector.dart
+    final FaceDetectorOptions faceDetectorOptions = FaceDetectorOptions(enableTracking: true, enableLandmarks: true, enableContours: false, enableClassification: true, minFaceSize: 0.1,mode: FaceDetectorMode.accurate);
+
+    ///agr criamos um FaceDetector e passamos o nosso faceDetectorOptions na instanciação
+    final FaceDetector faceDetector = FirebaseVision.instance.faceDetector(faceDetectorOptions);
+
+    ///aqui mandamos ele processar a nossa imagem
+    faceDetector.processImage(visionImage);
+
+    ///e aqui é o nosso resultado, temos elea apenas em logs, mas da pra passar pra view de algumas formas, depois vemos isso :)
+    final List<Face> faces = await faceDetector.processImage(visionImage);
+    print('Quantidade de Faces: ${faces.length}');
 
     for (Face face in faces) {
-      print('Teste For');
       var boundingBox = face.boundingBox;
-      final double rotY = face.headEulerAngleY; // Head is rotated to the right rotY degrees
-      final double rotZ = face.headEulerAngleZ; // Head is tilted sideways rotZ degrees
-      print('Teste For: ${face.smilingProbability}');
+      final double rotY = face.headEulerAngleY;
+      final double rotZ = face.headEulerAngleZ;
+      print('Rotação do Rosto: $rotY, $rotZ');
 
-      // If landmark detection was enabled with FaceDetectorOptions (mouth, ears,
-      // eyes, cheeks, and nose available):
-      final FaceLandmark leftEar = face.getLandmark(FaceLandmarkType.leftEar);
-      if (leftEar != null) {
-        var leftEarPos = leftEar.position as Point<double>;
-        print('leftEarPos: $leftEarPos');
-      }
-
-      // If classification was enabled with FaceDetectorOptions:
-      if (face.smilingProbability != null) {
-        var smileProb = face.smilingProbability;
-        print('SmileProb: $smileProb');
-      }
-
-      // If face tracking was enabled with FaceDetectorOptions:
       if (face.trackingId != null) {
         final int id = face.trackingId;
-        print('IDS: $id');
+        print('Rosto: $id');
       }
 
-      print('FACES TESTE: ${faces.elementAt(0).trackingId}');
+      final FaceLandmark leftEar = face.getLandmark(FaceLandmarkType.leftEar);
+      if (leftEar != null) {
+        var leftEarPos = leftEar.position;
+        print('posição da orelha esquerda: ${leftEarPos}');
+      }
+
+      if (face.smilingProbability != null) {
+        var smileProb = face.smilingProbability;
+        print('Sorrindo: $smileProb');
+      }
+
+
+      if (face.leftEyeOpenProbability != null){
+        var leftEyeOpen = face.leftEyeOpenProbability;
+        print('Olho Esquerdo aberto: $leftEyeOpen');
+      }
+      if (face.rightEyeOpenProbability != null){
+        var rightEyeOpen = face.rightEyeOpenProbability;
+        print('Olho Direito aberto: $rightEyeOpen');
+      }
     }
-
-
   }
-
-
 }
